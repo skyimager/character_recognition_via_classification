e3afccf2d8cadc8c66ed287e911bbdd6cc0b6d36
pradip.gupta <pradip.gupta@ril.com>
2019-04-10 22:33:23 +0530

    modified label map for 0-61

--- src/utils/chars_utility.py
+++ src/utils/chars_utility.py
@@ -10,9 +10,9 @@ def get_chars74k_label_map():
     # samples 1 through 10 are numbers '0' - '9'
     # samples 11 through 36 are uppercase letters
     # samples 37 through 62 are lowercase letters
-    num_start = 1
-    upper_start = 11
-    lower_start = 37
+    num_start = 0
+    upper_start = 10
+    lower_start = 36
 
     label_map = dict()
     for label, char in enumerate(string.digits, start = num_start):
@@ -25,3 +25,4 @@ def get_chars74k_label_map():
         label_map[label] = char
 
     return label_map
+
