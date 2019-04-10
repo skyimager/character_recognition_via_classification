df0ed251a032eea0a7d161fb04689eb0e101fe42
pradip.gupta <pradip.gupta@ril.com>
2019-04-10 11:43:25 +0530

    utility script for Chars74k dataset

--- src/utils/chars_utility.py
+++ src/utils/chars_utility.py
@@ -3,21 +3,25 @@
 import string
 
 def get_chars74k_label_map():
+    """
+    This function generates a label map for the chars74K datasetselfself.
+    This will help to display the true label from the predicted  class.
+    """
     # samples 1 through 10 are numbers '0' - '9'
     # samples 11 through 36 are uppercase letters
     # samples 37 through 62 are lowercase letters
     num_start = 1
     upper_start = 11
     lower_start = 37
-   
+
     label_map = dict()
     for label, char in enumerate(string.digits, start = num_start):
         label_map[label] = char
 
     for label, char in enumerate(string.ascii_uppercase, start = upper_start):
         label_map[label] = char
-    
+
     for label, char in enumerate(string.ascii_lowercase, start = lower_start):
         label_map[label] = char
-    
+
     return label_map
