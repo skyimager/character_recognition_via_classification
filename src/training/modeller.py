857ed4630b52b0b031a4e5137151713992254369
pradip.gupta <pradip.gupta@ril.com>
2019-04-15 22:42:16 +0530

    modified modeller to add dense layer

--- src/training/modeller.py
+++ src/training/modeller.py
@@ -1,13 +1,23 @@
-from src import config
+import config
+from keras.models import Model
+from keras.layers import Dense, Flatten, Dropout
 
 def finetune_model(base_model):
 
     for i, layer in enumerate(base_model.layers):
-        if i in config.trainable_layers:
-            layer.trainable=True
-        else:
+        if i in config.freeze_layers:
             layer.trainable=False
+        else:
+            layer.trainable=True
+    
+    x = base_model.output
+#    x = Flatten()(x)
+    x = Dense(256, activation='relu')(x)
+    x = Dropout(0.2)(x)
+    
+    predictions = Dense(config.nclasses, activation='softmax')(x) # New softmax layer
+    finetune_model = Model(inputs=base_model.input, outputs=predictions)
 
     print("Layer freezing complete!!")
 
-    return base_model
+    return finetune_model
